de6075d707d0c2a90dff6a0c26276e7b722a7591
Rudra <chaudhary66667@gmail.com>
2021-09-10 15:27:43 +0530

    Improved board design

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from board import logo,board
 print(logo)
-print(board)
+user_choice = input('Do you want to play the game press Yes ,Enter No to Exit\n').lower()
+
+if user_choice == 'yes':
+    user_input =  input('Enter a position from 1 to 9 on the board')
+
+
