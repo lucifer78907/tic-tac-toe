4360678cafe5b64ec1a491ed6334942c73857e64
Rudra <chaudhary66667@gmail.com>
2021-09-10 15:52:57 +0530

    Added postional args to board

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from board import logo,board
-print(logo)
-user_choice = input('Do you want to play the game press Yes ,Enter No to Exit\n').lower()
 
-if user_choice == 'yes':
-    user_input =  input('Enter a position from 1 to 9 on the board')
+board_list = ['' for _ in range(9)]
+print(board_list)
+print(logo)
+choice = input('Do you want to play the game press Yes ,Enter No to Exit\n').lower()
+user_icon = input('\nWhat you want to play with a "X" or a "O"\n').lower()
+comp_icon = ''
+if user_icon=='x':
+    comp_icon = 'o'
+else:
+    comp_icon = 'x'
+if choice == 'yes':
+    user_input = int(input('Enter a position from 1 to 9 on the board\n'))
+    board_list[user_input-1]=user_icon
+    print(board.format(*board_list))
 
 
